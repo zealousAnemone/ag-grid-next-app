581480483767df33ebafcccfc65c945d39f5932f
Veronica <34461546+zealousAnemone@users.noreply.github.com>
2022-05-02 13:15:39 -0400

    Update [id].js
    Add mock data if no connection to db.

--- pages/api/employee/[id].js
+++ pages/api/employee/[id].js
@@ -8,5 +8,23 @@ export default async function handler(req, res) {
     res.send(employee);
   } catch (error) {
     console.log(error);
+
+
+    /*
+     to support running locally for testing without setting up a database
+     enable the following code
+    */
+     if (error.code=== 'ECONNREFUSED' && process && process.env.NODE_ENV === 'development') {
+        res.json({"rows":[{
+          "id": id,
+          "first_name": "first" + id,
+          "last_name": "last" + id,
+          "email": "member" + id + "@company.com",
+          "phone": "12345" + id,
+          "office": "place" + id,
+          "job_title": "Worker " + id
+        }]})
+      }
+
   }
 }
