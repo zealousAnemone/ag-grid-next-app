bfe1daf31fec92996e754fae8e1faa8c079603c4
Veronica <34461546+zealousAnemone@users.noreply.github.com>
2022-05-02 13:17:03 -0400

    Update staff.js
    Creates fake data for when no db connection

--- pages/api/staff.js
+++ pages/api/staff.js
@@ -7,5 +7,26 @@ export default async function handler(req, res) {
     res.send(staff);
   } catch (error) {
     console.log(error);
+
+    /*
+     to support running locally for testing without setting up a database
+     enable the following code
+    */
+    if (error.code=== 'ECONNREFUSED' && process && process.env.NODE_ENV === 'development') {
+      const staff=[];
+
+      for (let id = 1; id <= 1000; id++) {
+        staff.push({
+          "id": id,
+          "first_name": "first" + id,
+          "last_name": "last" + id,
+          "email": "member" + id + "@company.com",
+          "phone": "12345" + id,
+          "office": "place" + id,
+          "job_title": "Worker " + id
+        })
+      }
+      res.json({"rows":staff});
+    }
   }
 }
